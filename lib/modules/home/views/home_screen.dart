461eaeab58826cde83936ecaa04690f698bc6c07
Ehtisham-logixloop <ehtisham.logixloop@gmail.com>
2026-07-11 12:44:33 +0500

    update code

--- lib/modules/home/views/home_screen.dart
+++ lib/modules/home/views/home_screen.dart
@@ -29,14 +29,6 @@ class _HomeScreenState extends State<HomeScreen> {
     CategoryModel(name: "Carpenter", images: "assets/images/carpenter.png"),
     CategoryModel(name: "Electrician", images: "assets/images/electrician.png"),
     CategoryModel(name: "Cleaner", images: "assets/images/cleaner.png"),
-    CategoryModel(name: "AC Repair", images: "assets/images/ac.png"),
-    CategoryModel(name: "Men’s saloon", images: "assets/images/salon.png"),
-    CategoryModel(name: "Painter", images: "assets/images/painter.png"),
-    CategoryModel(name: "Beauty", images: "assets/images/beauty.png"),
-    CategoryModel(name: "Automobile", images: "assets/images/automotive.png"),
-    CategoryModel(name: "Shifting", images: "assets/images/shifting.png"),
-    CategoryModel(name: "Home Appliance", images: "assets/images/appliances.png"),
-    CategoryModel(name: "Solar Install", images: "assets/images/home.png"),
   ];
 
   List<CategoryModel> filteredCategories = [];
